8578c1c55b7fc4ff9b9216d37b58267fe5a0d6fa
Jake Booher <jake.booher@gmail.com>
2021-04-27 15:29:37 -0500

    Add api middleware

--- src/LaravelAuth.php
+++ src/LaravelAuth.php
@@ -9,11 +9,14 @@ use Illuminate\Support\Facades\Route;
 
 class LaravelAuth
 {
-    public static function routes()
+    public static function routes(array $middleware = [])
     {
         Route::group([
             'prefix' => 'oauth',
             'namespace' => '\Audentio\LaravelAuth\Http\Controllers',
+            'middleware' => array_merge([
+                'api',
+            ], $middleware),
         ], function (Router $router) {
             Route::post('/token', 'AccessTokenController@issueToken')
                 ->middleware(InjectClientSecretForPasswordGrant::class);
